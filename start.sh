ce451974c4d5e07932839001202a03caa59a5ecd
Cyan Nyan <git@cyannyan.com>
2021-11-29 19:18:17 +0000

    Update velocity download api to paper

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Minecraft Server Launcher v1.1
-# For Minecraft 1.17.1
+# For Minecraft 1.17.1, Velocity 3.1.0, GeyserMC
 
 # set -xe
 
@@ -11,16 +11,23 @@ if [ -f "config.env" ]; then
   source "config.env"
 fi
 
+# > Example configs for different server types
+
 # SERVER=velocity
-# VERSION=3.0.1
+# VERSION=3.1.0
+
+# SERVER=paper
+# VERSION=1.17.1
+
+# SERVER=geyser
+# JVM_MEM="-Xms128M -Xmx128M"
+# JVM_PARAMS="-XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15"
 
 # === Change Options ===
-SERVER="${SERVER:-paper}"
-VERSION="${VERSION:-1.17.1}"
 JVM_MEM="${JVM_MEM:--Xms3G -Xmx3G}"
 JVM_PARAMS="${JVM_PARAMS:--XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true}"
 # AUTHLIB_INJECTOR=ely.by
-AUTHLIB_INJECTOR_VERSION=1.1.39
+AUTHLIB_INJECTOR_VERSION=1.1.40
 # ===   End Options   ===
 
 CACHE_DIR="cache"
@@ -35,8 +42,9 @@ fi
 
 case "$SERVER" in
     "velocity")
-        API_PATH="https://versions.velocitypowered.com/download"
+        API_PATH="https://papermc.io/api/v2/projects/velocity"
         RCON_CONFIG="plugins/velocityrcon/rcon.toml"
+        SERVER_JAR="velocity.jar"
         ;;
     "paper")
         API_PATH="https://papermc.io/api/v2/projects/paper"
@@ -60,7 +68,7 @@ _curl() {
         curl -fsSLo "$CACHE_DIR/$2.tmp" "${API_PATH}/$1"
         mv "$CACHE_DIR/$2.tmp" "$2"
     else
-        curl -fsS "${API_PATH}/$1"
+        curl -fsS "${API_PATH}$1"
     fi
 }
 
@@ -86,15 +94,6 @@ _paper_check_update() {
     ln -sf "$DOWNLOAD" "$SERVER_JAR"
 }
 
-_velocity_check_update() {
-    echo "Checking for velocity jar..."
-
-    if [ ! -f "$SERVER_JAR" ]; then
-        echo "Downloading $SERVER_JAR"
-        _curl "/$VERSION.jar" "$SERVER_JAR"
-    fi
-}
-
 _geyser_check_update() {
     echo "Checking for Geyser jar..."
 
@@ -116,12 +115,16 @@ _geyser_check_update() {
     ln -sf "$DOWNLOAD" "$SERVER_JAR"
 }
 
+_authlib_injector_check_update() {
+    if [ ! -f "$AUTHLIB_INJECTOR_JAR" ]; then
+        echo "Downloading $AUTHLIB_INJECTOR_JAR..."
+        curl -fsSLO "https://github.com/yushijinhun/authlib-injector/releases/download/v$AUTHLIB_INJECTOR_VERSION/$AUTHLIB_INJECTOR_JAR"
+    fi
+}
+
 _check_update() {
     case "$SERVER" in
-    "velocity")
-        _velocity_check_update
-        ;;
-    "paper")
+    "velocity" | "paper")
         _paper_check_update
         ;;
     "geyser")
@@ -130,10 +133,7 @@ _check_update() {
     esac
 
     if [ -n "$AUTHLIB_INJECTOR" ]; then
-      if [ ! -f "$AUTHLIB_INJECTOR_JAR" ]; then
-        echo "Downloading $AUTHLIB_INJECTOR_JAR..."
-        curl -fsSLO "https://github.com/yushijinhun/authlib-injector/releases/download/v$AUTHLIB_INJECTOR_VERSION/$AUTHLIB_INJECTOR_JAR"
-      fi
+        _authlib_injector_check_update
     fi
 }
 
@@ -191,7 +191,7 @@ main() {
             exit
             ;;
         "eula")
-            echo "eula=true" > "eula.txt"
+            > "eula.txt" <<< "eula=true"
             exit
             ;;
     esac
