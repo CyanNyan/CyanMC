0dc2ccba233d292817af11533f0c1540799a629c
Miracrypto <miracrypto@outlook.com>
2022-12-12 21:54:39 +0000

    Update config for 1.19.3

--- update_configs.sh
+++ update_configs.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-cp ../Matrix-Issues/{config,checks,language}.yml plugins/Matrix/
+# cp ../Matrix-Issues/{config,checks,language}.yml plugins/Matrix/
 cp -r ../Coins/src/main/resources/{language,config.yml} plugins/Coins/
 cp ../Essentials/Essentials/src/main/resources/config.yml plugins/Essentials/
